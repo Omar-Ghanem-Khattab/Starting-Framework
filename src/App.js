33e465a9044fe573764c9511d865f54453c3e2aa
unknown <omarghanem771012@gmail.com>
2023-10-06 19:46:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Layout from './Component/Layout/Layout'
+import About from './Component/About/About'
+import Portfolio from './Component/Portfolio/Portfolio'
+import Contact from './Component/Contact/Contact'
+import Home from './Component/Home/Home'
+import NotFound from './Component/Notfound/NotFound'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+
+export default function App() {
+
+  const routers = createHashRouter([
+    {
+      path: '', element: <Layout />, children: [
+        { index: true, element: <Home></Home> },
+        { path: "about", element: <About></About> },
+        { path: "portfolio", element: <Portfolio></Portfolio> },
+        { path: "contact", element: <Contact></Contact> },
+        { path: '*', element: <NotFound></NotFound> }
+      ]
+    }
+  ])
+
+  return (
+    <>
+      <RouterProvider router={routers}></RouterProvider>
+    </>
+  )
+}
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
+reportWebVitals();
